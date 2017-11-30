f2b17362597cb7ea6f7be1ec20cb91115b4680a4
zodiacfireworks <martin.vuelta@gmail.com>
2017-11-29 19:40:04 -0500

    Udate example 2

---
+++ Session 01/session_1/02_tpk_f95/tpk.f95
@@ -0,0 +1,27 @@
+module Functions
+    public :: fun
+    contains
+    function fun(t) result (r)
+        real, intent(in) :: t
+        real  :: r
+        r = sqrt(abs(t)) + 5.0*t**3
+    end function fun
+end module Functions
+
+program TPK
+    ! The TPK Algorithm
+    ! F style
+    use Functions
+    integer :: i
+    real    :: y
+    real, dimension(0:10) :: a
+    read *, a
+    do i = 10, 0, -1      ! Backwards
+       y = fun(a(i))
+       if ( y < 400.0 ) then
+          print *, i, y
+       else
+          print *, i, " Too large"
+       end if
+    end do
+end program TPK
